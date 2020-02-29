5db5bdaa62b35d3a6477bb2fef0b17c8916dfff5
Sakanoharuna <kismyftshft@gmail.com>
2020-02-29 20:36:17 +0900

    cssの微修正

--- resources/views/user/shows.blade.php
+++ resources/views/user/shows.blade.php
@@ -7,7 +7,12 @@
             </div>
             <ul class="user_info">
                 <div class="vacant-link">
-                    <a href="{{ route('vacant.create',['id' => \Auth::user()->id])}}">空いてる日を登録する！</a>
+                    <p>空いてる日を登録する！</p>
+                    <a href="{{ route('vacant.create',['id' => \Auth::user()->id])}}">登録フォーム</a>
+                </div>
+                <div class="share-link">
+                    <p>空いてる日をシェアする！</p>
+                    <a href="#">共有用リンク</a>
                 </div>
             </ul>
         </section>
